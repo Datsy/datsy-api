6cdb63030130c3a30552970847d113d66665160b
jokomo <joannemta@gmail.com>
2013-11-25 13:06:29 -0800

    updated to allow for secure conString

--- server/helpers/cloudstorage.js
+++ server/helpers/cloudstorage.js
@@ -1,29 +1,35 @@
 var azure = require('azure');
 
+// ask Emma for config file
+var conString = require('./azurecloudconfig.json').storage.conString; 
 var retryOperations = new azure.ExponentialRetryPolicyFilter();
-var blobService = azure.createBlobService().withFilter(retryOperations);
+var blobService = azure.createBlobService(conString).withFilter(retryOperations);
 // upload a block blob to cloud
-// blobService.createBlockBlobFromFile('csv'
-// , 'rawdata1.txt'
-// , 'rawdata.txt'
-// , function(error){
-//     if(!error){
-//         // File has been uploaded
-//     }
-// });
+
+blobService.createBlockBlobFromFile('csv'
+, 'rawdata1.txt'
+, 'rawcsv/cat_rawdata.csv'
+, function(error){
+    if(!error){
+        // File has been uploaded
+    }
+});
 
 // how to download blobs
-// var fs=require('fs');
-// blobService.getBlobToStream('csv'
-// , 'rawdata.txt'
-// , fs.createWriteStream('output/outputtest.txt')
-// , function(error){
-//     if(!error){
-//         // Wrote blob to stream
-//     }
-// });
 
-delete blob
+var fs=require('fs');
+blobService.getBlobToStream('csv'
+, 'rawdata.txt'
+, fs.createWriteStream('output/outputtest.txt')
+, function(error){
+    if(!error){
+        // Wrote blob to stream
+    }
+});
+
+// delete blob
+
+
 blobService.deleteBlob('csv'
 , 'rawdata1.txt'
 , function(error){
