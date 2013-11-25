f8eb6a6ba5bcbe2413192060da77219215cdc263
jokomo <joannemta@gmail.com>
2013-11-25 09:33:33 -0800

    made chunking of 500

--- server/helpers/csvtopostgres.js
+++ server/helpers/csvtopostgres.js
@@ -2,61 +2,13 @@ var pg = require('pg');
 var fs = require('fs');
 
 var credentials = require('./dbconfig.json').db;
-var conString = "postgres://"+credentials.user+":" +credentials.password+ "@137.135.14.92/postgres";
+var conString = "postgres://"+credentials.user+":" +credentials.password+ "@" + credentials.localhost + "/" + credentials.dbname;
 var client = new pg.Client(conString);
 var dir='./rawcsv/';
 
-var insertDB = function (createQS, insertQS) {
-  // client.end();
-  client.connect(function(err) {
-    if(err) {
-      return console.error('could not connect to postgres', err);
-    }
-    // client.query(createQS, function(err, result) {
-    //   if(err) { return console.error('error with creation', err);}
-    //   console.log('created table');
-    // });
-    client.query(insertQS, function(err, result) {
-      if(err) { return console.error('error with insertion', err);}
-      console.log('inserted data into');
-      client.end();
-    });
-  });
-};
-
-var createQString = function(tablename, col_names, col_types, col_values) {
-  var num_col = col_names.length;
-  var createQS = 'CREATE TABLE IF NOT EXISTS '+ tablename +' (';//ID INT PRIMARY KEY NOT NULL
-  var insertQS = 'INSERT INTO ' + tablename+ ' (' + col_names.toString() + ')' + ' VALUES '
-
-  for (var i = 0; i < num_col; i++) {
-    createQS += col_names[i] + ' ' + col_types[i] + ' NOT NULL';
-    if (i < num_col - 1) {createQS += ',';}
-  }
-  var num_row = col_values.length;
-
-  for (var i = 0; i < num_row; i++) {
-    insertQS += ' (\'';
-    insertQS += col_values[i].replace(/[^A-Za-z\s\d,&://]/g, '').split(',').join('\',\'');
-    insertQS += '\')';
-    if (i < num_row - 1) {
-      insertQS += ',';
-    }
-  }
-  createQS += ');';
-  insertQS += ';';
-  insertDB(createQS, insertQS);
-};
-
-var readOne = function (filepath) {
-    var data = fs.readFileSync('./rawcsv/2013-3rd-quarter.csv', 'utf8');
-    var array = data.split('\n');
-    var tablename = array[0];
-    var col_names = array[1].split(',');
-    var col_types = array[2].split(',');
-    var col_values = array.slice(3);
-    createQString(tablename, col_names, col_types, col_values);
-};
+var rowlimit = 500;
+var num_row = 0;
+var table = {};
 
 var readFolder = function () {
   fs.readdir(dir,function(err,files){
@@ -69,6 +21,74 @@ var readFolder = function () {
     });
   });
 };
+var readOne = function (filepath) {
+    var data = fs.readFileSync('./rawcsv/2013-3rd-quarterfull.csv', 'utf8');
+    var array = data.split('\n');
+    table.tablename = array[0];
+    table.col_names = array[1].split(',');
+    table.col_types = array[2].split(',');
+    table.col_values = array.slice(3);
+    table.num_col = table.col_names.length;
+    table.num_row = table.col_values.length;
+    createQString();
+};
+var createQString = function() {
+  var createQS = 'CREATE TABLE IF NOT EXISTS '+ table.tablename +' (';//ID INT PRIMARY KEY NOT NULL
+  for (var i = 0; i < table.num_col; i++) {
+    createQS += table.col_names[i] + ' ' + table.col_types[i] + ' NOT NULL';
+    if (i < table.num_col - 1) {createQS += ',';}
+  }
+  createQS += ');';
+  createDB(createQS);
+};
+
+
+var createDB = function (createQS, insertQS) {
+  client.connect(function(err) {
+    if(err) {
+      return console.error('could not connect to postgres', err);
+    }
+    client.query(createQS, function(err, result) {
+      if(err) { return console.error('error with creation', err);}
+      console.log('created table');
+      insertDB(848000 + 500);
+    });
+  });
+};
+
+var insertDB = function (startRow) {
+  if (startRow >= table.num_row) {
+    client.end();
+    return;
+  }
+
+  var insertQS = '';
+  insertQS = 'INSERT INTO ' + table.tablename+ ' (' + table.col_names.toString() + ')' + ' VALUES ';
+  if (startRow + rowlimit >= table.num_row) {
+    rowlimit = table.num_row - startRow;
+  }
+  for (var i = startRow; i < startRow + rowlimit; i++) {
+    insertQS += ' (\'';
+    insertQS += table.col_values[i].replace(/[^A-Za-z\s\d,&://]/g, '').split(',').join('\',\'');
+    insertQS += '\')';
+    if (i < startRow + rowlimit - 1) {
+      insertQS += ',';
+    }
+  }
+  insertQS += ';';
+
+  fs.writeFile('./output.txt', insertQS, function() {});
+  client.query(insertQS, function(err, result) {
+    if (err) { return console.error('error with insertion', err);}
+    console.log('inserted data into starting from ' + startRow);
+    insertDB(startRow + rowlimit);
+    if (startRow + rowlimit >= table.num_row) {
+      console.log('done!!');
+      client.end();
+    }
+  });
+}
+
 readOne();
 
 
