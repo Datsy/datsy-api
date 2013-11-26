a18694abb6b69f3db135f197ad346630aae772f8
shanshan <shanshan@shanshans-MacBook-Pro-2.local>
2013-11-26 15:21:47 -0800

    tested working for VM in scrapping SF location_demographic data

--- dataScrapper/factualGeopulse/MongoDB_CSV_helperFunctions.js
+++ dataScrapper/factualGeopulse/MongoDB_CSV_helperFunctions.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 
+
 var getAllKeys = function(obj,keysArr,vals, keychain) {
   var keysArr = keysArr || [];
   var vals = vals || [];
@@ -11,18 +12,44 @@ var getAllKeys = function(obj,keysArr,vals, keychain) {
     var key = keys[i];
     var val = obj[key];
     if (val && Array.prototype.toString.call(val) === '[object Object]' && Object.keys(val).length !== 0) {
-      getAllKeys(val, keysArr, vals, keychain + key + '-');//'_' is the separator between parent and kid keys
+      getAllKeys(val, keysArr, vals, keychain + key + '__');//'_' is the separator between parent and kid keys
     } else {
       keysArr.push(keychain + key);
       if (typeof val === 'number') {
         vals.push(val);
       } else {
-        vals.push('');//replace null, undefined or {}
+        vals.push("null");//replace null, undefined or {}
       }
     }
   }
   return[keysArr, vals];
 };
+
+
+var checkKeys = function(keyvals, checkKeys) {
+  var parsedVals = [];
+  var keys = keyvals[0];
+  var vals = keyvals[1];
+  var found = false;
+  for (var i = 0; i < checkKeys.length; i ++) {
+    for (var j = 0; j < keys.length; j ++) {
+      if(checkKeys[i] === keys[j]) {
+        parsedVals.push(vals[j]);
+        found = true;
+        break;
+      }
+    }
+    if (found) {
+        found = false;
+        continue;
+    }
+    if (!found) {
+      parsedVals.push("null");//must be a string rather than null val.
+      found = false;
+    }
+  } 
+    return parsedVals;
+}
 
 var checkCSVFolder = function(csvFolder) {
   fs.exists(csvFolder, function(exists){
@@ -34,14 +61,22 @@ var checkCSVFolder = function(csvFolder) {
 
 exports.checkCSVFolder = checkCSVFolder;
 exports.getAllKeys = getAllKeys;
+exports.checkKeys = checkKeys;
+
 
 // var testObj = 
-//   { demographics: 
+//       { area_statistics: {a:34,b:{bc:5}},
+//         income: undefined,
+
+//         age_and_sex: {a:333,c:444}  } ;
+        
+// var checkObj = 
 //       { area_statistics: {a:34,b:{ac:3,bc:5}},
 //         income: undefined,
 //         housing: 4,
 //         education: null,
 //         race_and_ethnicity: {},
-//         age_and_sex: {a:333,c:444}  } };
-// console.log(getAllKeys(testObj)[1].join(','));
-
+//         age_and_sex: {a:333,c:444}  } ;
+        
+// var a = checkKeys(getAllKeys(testObj),getAllKeys(checkObj)[0]);
+// console.log(a);
--- dataScrapper/factualGeopulse/csvtopostgres_nullStr.js
+++ dataScrapper/factualGeopulse/csvtopostgres_nullStr.js
@@ -31,16 +31,19 @@ var readOne = function (filepath) {
     table.col_values = array.slice(3);
     table.num_col = table.col_names.length;
     table.num_row = table.col_values.length;
+    console.log(table.num_col);
+    console.log(table.num_row);
     createQString();
 };
 var createQString = function() {
   var createQS = 'CREATE TABLE IF NOT EXISTS '+ table.tablename +' (';//ID INT PRIMARY KEY NOT NULL
   for (var i = 0; i < table.num_col; i++) {
-    createQS += table.col_names[i] + ' ' + table.col_types[i] + ' NOT NULL';
+    createQS += table.col_names[i] + ' ' + table.col_types[i];
     if (i < table.num_col - 1) {createQS += ',';}
   }
   createQS += ');';
   createDB(createQS);
+  console.log(createQS);
 };
 
 
@@ -52,16 +55,18 @@ var createDB = function (createQS, insertQS) {
     client.query(createQS, function(err, result) {
       if(err) { return console.error('error with creation', err);}
       console.log('created table');
-      insertDB(848000 + 500);
+      insertDB(0);
     });
   });
 };
 
 var insertDB = function (startRow) {
+  console.log('insert starts');
   if (startRow >= table.num_row) {
     client.end();
     return;
   }
+  var temp = [];
 
   var insertQS = '';
   insertQS = 'INSERT INTO ' + table.tablename+ ' (' + table.col_names.toString() + ')' + ' VALUES ';
@@ -70,15 +75,33 @@ var insertDB = function (startRow) {
   }
   for (var i = startRow; i < startRow + rowlimit; i++) {
     insertQS += ' (\'';
-    insertQS += table.col_values[i].replace(/[^A-Za-z\s\d,&:-//]/g, '').split(',').join('\',\'');
+    insertQS += table.col_values[i].replace(/[^A-Za-z\s\d,&:.\-//]/g, '').split(',').join('\',\'');
     insertQS += '\')';
+//for insertion into Postgres, must use "null", but not 'null'
+
+    // insertQS += ' (;
+    // var arr = table.col_values[i].replace(/[^A-Za-z\s\d,&:.\-//]/g, '').split(',');
+    // for (var j = 0; j < arr.length; j ++) {
+    //   if (arr[j] === '') {
+    //     insertQS += "null";
+    //   } else {
+    //     insertQS += "'" +arr[j] +"'";
+    //   }
+    //   if (j < arr.length -1) {
+    //     insertQS += ',';
+    //   }
+    // }
+    // temp.push(arr.length);
+//    insertQS += ')';
+
     if (i < startRow + rowlimit - 1) {
       insertQS += ',';
     }
   }
+  insertQS = insertQS.replace(/\'null\'/g,'null');
   insertQS += ';';
 
-  // fs.writeFile('./output.txt', insertQS, function() {});
+  fs.writeFile('./output.txt', insertQS, function() {});
   client.query(insertQS, function(err, result) {
     if (err) { return console.error('error with insertion', err);}
     console.log('inserted data into starting from ' + startRow);
