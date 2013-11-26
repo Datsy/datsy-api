ab68155cce6f59dd6a3946776c0ee00656a3b7f4
pieteradejong <pieter.a.dejong@gmail.com>
2013-11-26 15:57:22 -0800

    added metadata handlers

---
+++ server/helpers/metadataDb.js
@@ -0,0 +1,94 @@
+module.exports = {
+
+  saveDatasetInDb : function(jsonMetadata) {
+    
+    var dataset         = new Dataset();
+    dataset.url         = jsonMetadata.url;
+    dataset.name        = jsonMetadata.name;
+    dataset.title       = jsonMetadata.title;
+    dataset.description = transformForPostgres(jsonMetadata.description);
+    dataset.author      = jsonMetadata.author;
+    dataset.user_id     = jsonMetadata.user_id;
+    dataset.numcols     = jsonMetadata.numcols;
+    
+    dataset.save(function(err, dataset) {
+      if(err) {
+        console.log(err);
+      } else {
+        saveDatasetColumnsInDb(dataset);  
+      }
+    });
+  },
+
+  saveDatasetInDbFromFile : function(filepath) {
+    var json = require(filepath);
+    saveDatasetInDb(json);
+  },
+
+  saveDatasetColumnsInDb : function(dataset) {
+    for(var i = 0; i < dataset.columns.length; i++) {
+      var column = dataset.columns[i];
+      DatasetColumns.create({
+        table_id: dataset.id,
+        colName: column.name,
+        colDatatype: column.datatype,
+        colMeaning: column.meaning
+      }, function(err, data) {
+        if(err) {
+          console.log(err);
+        } else {
+          return data;
+        }
+      });
+    }
+  },
+
+  getColumnsByDatatype : function(datatypeString) {
+    return DatasetColumns.all({colDatatype: datatypeString}, function(err, DatasetColumns){
+      if(err) {
+        console.log(err);
+      } else {
+        return DatasetColumns;
+      }
+    });
+  },
+
+  getDatasetsByTagName : function(tagName) {
+    // tagName -> array of Datasets
+    var tag = getTagByName(tagName);
+    return DataTags.all({where: {id: tag.id, dataCategory: 'set'}}, function(err, DatasetIds){
+      if(err) {
+        console.log(err);
+      } else {
+        return Datasets.all({id: DatasetIds});
+      }
+    });
+  },
+
+  getColumnsByTagName : function(tagName) {
+    var tag = getTagByName(tagName);
+    var tagId = tag.id;
+    return DataTags.all({where: {id: tagId, dataCategory: 'column'}}, function(err, ColumnIds){
+      if(err) {
+        console.log(err);
+      } else {
+        return DatasetColumns.all({id: ColumnIds});
+      }
+    });
+  },
+  
+  getTagByName : function(tagName) {
+    return Tags.findOne({where: {label: tagName}});
+  }
+
+  /*
+    Helpers
+  */
+  transformForPostgres : function(string) {
+    return string.replace("\n", " ");
+  }
+}
+// should be able to:
+// search by name, column, tags
+// search for particular data set meta data based on id
+// search based on tag, or column type
---
+++ server/models/metadataModel.js
@@ -0,0 +1,63 @@
+var credentials = require('../config/dbconfig.json').db;
+var Schema = require('jugglingdb').Schema;
+var schema = new Schema('postgres', {
+  database: 'metadata3',
+  username: credentials.user,
+  host: credentials.localhost,
+  password: credentials.password
+});
+
+var Dataset = schema.define('datasets', {
+  url: { type: String, length: 255 },
+  name: { type: String, length: 255 },
+  title: { type: String, length: 255 },
+  description: { type: Schema.Text },
+  author: { type: String, length: 255 },
+  date_added: { type: Date, default: function() { return new Date; } },
+  numrows: { type: Number },
+  numcols: { type: Number }
+});
+
+var DatasetColumns = schema.define('datasetcolumns', {
+  dataset_id: { type: Number }, // fk
+  col_name: { type: String, length: 45 },
+  col_datatype: { type: String, length: 45 },
+  col_meaning: { type: Schema.Text }
+});
+
+var Tags = schema.define('tags', {
+  label: { type: String, length: 45 }
+});
+
+var DataTags = schema.define('datatags', {
+  data_category: { type: String, length: 15 },
+  data_id: { type: Number },
+  tag_id: { type: Number } // fk
+});
+  
+var json = require('../metadata.json');
+var md = require('../helpers/metadataDb');
+md.saveDatasetInDb(json);
+
+// Dataset.hasMany(DatasetColumns, {as: 'column', foreignKey: 'tableId' });
+
+// ALTER TABLE datasetcolumns ADD FOREIGN KEY (dataset_id) REFERENCES datasets;
+
+// DatasetColumns.belongsTo(Dataset, {as: 'dataset', foreignKey: 'tableId'});
+
+// Tags.hasMany(DataTags, {as: 'datatag', foreignKey: 'tagId'});
+// DataTags.hasOne(Tags, {as: 'tag', foreignKey: 'tagId'});
+
+// ALTER TABLE datatags ADD FOREIGN KEY (tag_id) REFERENCES tags;
+
+schema.autoupdate(function(msg){
+  console.log("*** db schema update is done", msg);
+  // Dataset.create({url: 'laladifjsdljf45452'});
+    // var dbset = new Dataset({
+    //   url: 'blah'
+    // });
+
+    // dbset.save(function(err) {
+    //   if (err) console.log('there is a major erro!!');
+    // });
+});
