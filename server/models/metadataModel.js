bd95be79ddc4378ce2a6b331da0a778bd2e80300
pieteradejong <pieter.a.dejong@gmail.com>
2013-11-26 16:51:22 -0800

    adds helper-functions to metadata model

--- server/models/metadataModel.js
+++ server/models/metadataModel.js
@@ -37,6 +37,97 @@ var metadataModel = function(schema) {
   Metadata.Dataset.hasMany(Metadata.DataColumn, {as: 'datacolumn', foreignKey: 'dataset_id'});
 
 
+  // define helper functions
+
+
+  Metadata.prototype.saveDatasetInDb = function(jsonMetadata) {
+    
+    var dataset         = new Dataset();
+    dataset.url         = jsonMetadata.url;
+    dataset.name        = jsonMetadata.name;
+    dataset.title       = jsonMetadata.title;
+    dataset.description = transformForPostgres(jsonMetadata.description);
+    dataset.author      = jsonMetadata.author;
+    dataset.user_id     = jsonMetadata.user_id;
+    dataset.numcols     = jsonMetadata.numcols;
+    
+    dataset.save(function(err, dataset) {
+      if(err) {
+        console.log(err);
+      } else {
+        saveDatasetColumnsInDb(dataset);  
+      }
+    });
+  },
+
+  Metadata.prototype.saveDatasetInDbFromFile = function(filepath) {
+    var json = require(filepath);
+    saveDatasetInDb(json);
+  },
+
+  Metadata.prototype.saveDatasetColumnsInDb = function(dataset) {
+    for(var i = 0; i < dataset.columns.length; i++) {
+      var column = dataset.columns[i];
+      DatasetColumns.create({
+        table_id: dataset.id,
+        colName: column.name,
+        colDatatype: column.datatype,
+        colMeaning: column.meaning
+      }, function(err, data) {
+        if(err) {
+          console.log(err);
+        } else {
+          return data;
+        }
+      });
+    }
+  },
+
+  Metadata.prototype.getColumnsByDatatype = function(datatypeString) {
+    return DatasetColumns.all({colDatatype: datatypeString}, function(err, DatasetColumns){
+      if(err) {
+        console.log(err);
+      } else {
+        return DatasetColumns;
+      }
+    });
+  },
+
+  Metadata.prototype.getDatasetsByTagName = function(tagName) {
+    // tagName -> array of Datasets
+    var tag = getTagByName(tagName);
+    return DataTags.all({where: {id: tag.id, dataCategory: 'set'}}, function(err, DatasetIds){
+      if(err) {
+        console.log(err);
+      } else {
+        return Datasets.all({id: DatasetIds});
+      }
+    });
+  },
+
+  Metadata.prototype.getColumnsByTagName = function(tagName) {
+    var tag = getTagByName(tagName);
+    var tagId = tag.id;
+    return DataTags.all({where: {id: tagId, dataCategory: 'column'}}, function(err, ColumnIds){
+      if(err) {
+        console.log(err);
+      } else {
+        return DatasetColumns.all({id: ColumnIds});
+      }
+    });
+  },
+  
+  Metadata.prototype.getTagByName = function(tagName) {
+    return Tags.findOne({where: {label: tagName}});
+  }
+
+  /*
+    Helpers
+  */
+  Metadata.prototype.transformForPostgres = function(string) {
+    return string.replace("\n", " ");
+  }
+
   return Metadata;
 };
 
