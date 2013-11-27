f5b27f9ec185f43197d36dec40268657bd6e42e3
bradfordh <bradfordh@gmail.com>
2013-11-27 09:31:49 -0800

    Small refactoring

--- server/models/metadataModel.js
+++ server/models/metadataModel.js
@@ -9,9 +9,9 @@ var metadataModel = function(schema) {
   Metadata.Dataset = schema.define('dataset', {
     table_name: {type: String, unique: true},
     url: {type: String},
-    title: {type: String, length: 45},
+    title: {type: String},
     description: {type: String},
-    author: {type: String, length: 45},
+    author: {type: String},
     date_added: {type: Date},
     row_count: {type: Number},
     col_count: {type: Number}
@@ -19,20 +19,20 @@ var metadataModel = function(schema) {
 
 
   Metadata.Tag = schema.define('tag', {
-    label: {type: String, length: 45}
+    label: {type: String, unique: true}
   });
 
 
   Metadata.DataColumn = schema.define('datacolumn', {
-    name: {type: String, length: 45},
-    datatype: {type: String, length: 45},
+    name: {type: String},
+    datatype: {type: String},
     description: {type: String}
   });
 
 
   // Create the table relationships
 
-  Metadata.Dataset.hasAndBelongsToMany(Metadata.Tag, {as: 'tag', foreignKey: 'dataset_id'});
+  Metadata.Dataset.hasAndBelongsToMany(Metadata.Tag, {as: 'tag', foreignKey: 'tag_id'});
 
   Metadata.Dataset.hasMany(Metadata.DataColumn, {as: 'datacolumn', foreignKey: 'dataset_id'});
 
@@ -47,13 +47,14 @@ var metadataModel = function(schema) {
   Metadata.saveDataset = function(jsonMetadata) {
 
     var dataset         = new this.Dataset();
+    dataset.table_name  = jsonMetadata.table_name;
     dataset.url         = jsonMetadata.url;
     dataset.name        = jsonMetadata.name;
     dataset.title       = jsonMetadata.title;
     dataset.description = this.transformForPostgres(jsonMetadata.description);
     dataset.author      = jsonMetadata.author;
-    dataset.user_id     = jsonMetadata.user_id;
-    dataset.numcols     = jsonMetadata.numcols;
+    dataset.col_count   = jsonMetadata.num_cols;
+    dataset.date_added  = jsonMetadata.date_added;
 
     var self = this;
     dataset.save(function(err, dataset) {
@@ -82,7 +83,7 @@ var metadataModel = function(schema) {
       dataset.datacolumn.create({
         name: column.name,
         datatype: column.datatype,
-        description: column.meaning
+        description: column.description
       }, function(err, data) {
         if(err) {
           console.log(err);
@@ -94,49 +95,6 @@ var metadataModel = function(schema) {
   };
 
 
-  // Gets all columns containing a certain datatype
-
-  Metadata.getColumnsByDatatype = function(datatypeString, cb) {
-    return this.DataColumn.all({where: {datatype: datatypeString}}, cb);
-  };
-
-
-  // Gets all datasets with a certain tag
-
-  Metadata.getDatasetsByTagName = function(tagName) {
-    // tagName -> array of Datasets
-    var tag = getTagByName(tagName);
-    return DataTags.all({where: {id: tag.id, dataCategory: 'set'}}, function(err, DatasetIds){
-      if(err) {
-        console.log(err);
-      } else {
-        return Datasets.all({id: DatasetIds});
-      }
-    });
-  };
-
-
-  // Gets all columns with a certain tag
-
-  Metadata.getColumnsByTagName = function(tagName) {
-    var tag = getTagByName(tagName);
-    var tagId = tag.id;
-    return DataTags.all({where: {id: tagId, dataCategory: 'column'}}, function(err, ColumnIds){
-      if(err) {
-        console.log(err);
-      } else {
-        return DatasetColumns.all({id: ColumnIds});
-      }
-    });
-  };
-
-
-  // Returns the tab object for a given name
-
-  Metadata.getTagByName = function(tagName) {
-    return Tags.findOne({where: {label: tagName}});
-  }
-
   // Return new lines from string
 
   Metadata.transformForPostgres = function(string) {
@@ -147,5 +105,5 @@ var metadataModel = function(schema) {
   return Metadata;
 };
 
-  module.exports = metadataModel;
+module.exports = metadataModel;
 
