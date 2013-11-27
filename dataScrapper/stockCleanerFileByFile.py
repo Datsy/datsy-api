fe5e89db6384d5c9a2a3863ad3d10569266bf28d
shanshan <shanshan@shanshans-MacBook-Pro-2.local>
2013-11-27 12:30:39 -0800

    add stockCleanerFilebyFile

--- dataScrapper/stockCleanerFileByFile.py
+++ dataScrapper/stockCleanerFileByFile.py
@@ -5,24 +5,23 @@
 #clean and merge data in one csv 
 from os import listdir
 from os.path import isfile, join
+# mypath = './USstockHistory167Mb/'
 mypath = './USstockHistory167Mb/'
 onlyfiles = [ f for f in listdir(mypath) if isfile(join(mypath,f)) and f[-3:] == 'csv'  ]
-print onlyfiles
 # for f in onlyfiles:
 # 	fin = open(mypath + f)
 
-ticker = 'blh'
+outputFolder = './USstockHistory167Mb_output/'
 
-foutfinal = open('./USstockHistory167Mb_output/stockdata.csv', 'w')
-foutmeta = open('./USstockHistory167Mb_output/stockdatameta.csv', 'w')
+foutmeta = open(outputFolder + 'stockdatameta.csv', 'a')
 
 def firstheader(line):
-	global ticker
 	start = line.index('[') + 1
 	end = line.index(']')
 	ticker = line[start:end]
 	foutmeta.write(line) # get the stock ticker and description
 	foutfinal.write('stockdata_84_06_eod' + '\n')
+	return ticker
 
 def secondheader(line):
 	csvlist = line.replace('# ', '').split(' ')
@@ -36,28 +35,24 @@ def secondheader(line):
 		foutfinal.write(',FLOAT4')
 	foutfinal.write('\n')
 
-def findTicker(line):
-	global ticker
-	start = line.index('[') + 1
-	end = line.index(']')
-	ticker = line[start:end]
-
-
-for index,f in enumerate(onlyfiles):
-	fin = open(mypath + onlyfiles[index], 'r')
-	if index == 0:
-		firstheader(fin.readline())
-		secondheader(fin.readline())
-	else:
-		findTicker(fin.readline())
-		fin.next()
-	for line in fin:
-		foutfinal.write(ticker + ',' + line)
-	fin.close()
-
-
-
-
-
-# fin = open()
-# print onlyfiles
+for f in onlyfiles:
+  fin = open(mypath + f, 'r')
+  lines = fin.readlines()
+  fOutPath = outputFolder + f
+  ticker = ''
+  if isfile(fOutPath):
+  	print fOutPath + 'exists!'
+  else:
+	  with open(fOutPath, 'a') as foutfinal:
+		  for index,line in enumerate(lines):
+		  	if index == 0:
+		  		ticker = firstheader(line)
+		  		print ticker
+		  	elif index == 1:
+		  		secondheader(line)
+		  	else:
+		  		if index == len(lines) - 1:
+		  			foutfinal.write(ticker+','+line.rstrip('\n'))
+		  		else:
+			  		foutfinal.write(ticker+','+line)
+  fin.close()
