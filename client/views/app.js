dab971807ab13fa92eb068cdd018c65dba885d52
bradfordh <bradfordh@gmail.com>
2013-11-28 20:32:27 -0500

    Removes unneded console.logs()

--- client/views/app.js
+++ client/views/app.js
@@ -1,4 +1,3 @@
-// (function($){
 var Router = Backbone.Router.extend({
   routes: {
     "posts/:id": "getPost", // will match example: /#/posts/1
@@ -16,38 +15,32 @@ var Router = Backbone.Router.extend({
 
 var Datsy = Backbone.View.extend({
   initialize: function(){
-    console.log("Datsy is running");
-    console.log("Datsy is still running");
+    // TODO: Fill in details here
   },
 
   userLogin: function(className){
-    console.log("In login");
     this.pageView = new LoginView();
     $('.container .section1').html(this.pageView.render().el);
   },
 
   userSignup: function(className){
-    console.log("In Signup");
     this.signupView = new SignupView();
     $('.container .section1').html(this.signupView.render().el);
   },
 
   userVerifyEmail: function(className){
-    console.log("In verify Email");
     this.verifyEmail = new VerifyEmailView();
     $('.container .section1').html(this.verifyEmail.render().el);
   },
 
   aboutUs: function(){
-    console.log("In About Us");
     this.aboutUsView = new AboutUsView();
-    $('.container .section1').html(this.aboutUsView.render().el);  
+    $('.container .section1').html(this.aboutUsView.render().el);
   }
 });
 
 var app_router = new Router();
 var datsy = new Datsy();
-console.log("router initialized");
 Backbone.history.start();
 
 app_router.on('route:login', datsy.userLogin);
@@ -56,9 +49,7 @@ app_router.on('route:verifyEmail', datsy.userVerifyEmail);
 app_router.on('route:aboutUs', datsy.aboutUs);
 app_router.on('route:getPost', function (id) {
     // Note the variable in the route definition being passed in here
-    console.log("In posts");
     alert( "Get post number " + id );
 });
 
 
-// })(jQuery);
