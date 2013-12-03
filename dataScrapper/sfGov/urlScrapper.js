46a67de15006262f3f2aad1d1a0ef67462f378d8
Hack Reactor Students <github@hackreactor.com>
2013-12-02 22:57:27 -0800

    add a few comments

--- dataScrapper/sfGov/urlScrapper.js
+++ dataScrapper/sfGov/urlScrapper.js
@@ -4,7 +4,7 @@ var dataDownloader = require('../helper/dataDownloader.js').dataDownloader;
 
 
 var getURL = function (tag, pageUrl) {
-  $('.sidebarOptionsContainer').find('a.export').trigger('click');
+  $('.sidebarOptionsContainer').find('a.export').trigger('click');//this must be triggered in domReady.
   $cellInner = $('.cellInner');
   return $cellInner.length <= 1? '' : $cellInner.find("[data-type=" + tag + "]").attr('href');
 };
@@ -12,6 +12,8 @@ var getURL = function (tag, pageUrl) {
 var downLoad = function() {
   var title = $('#infoBox').find('.clipText').attr('title');
   title = title.replace(/[^A-Za-z\d/_]/g, '_');
+  
+  //csv for original formated file, json for metadata(raw);
   sfGovDownLoad(getURL('CSV'), title, '.csv', function(){
     sfGovDownLoad(getURL('CSV'), title, '.json');
   });
