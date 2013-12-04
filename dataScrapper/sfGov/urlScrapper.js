edfdde51aa8d754eb88c2d70d3a948da83b93812
Hack Reactor Students <github@hackreactor.com>
2013-12-04 14:19:13 -0800

    fix & and replace\ error, used successfully to scrap data from sfgov

--- dataScrapper/sfGov/urlScrapper.js
+++ dataScrapper/sfGov/urlScrapper.js
@@ -12,33 +12,28 @@ var startUrl = "http://data.sfgov.org/browse?limitTo=datasets&utf8=%E2%9C%93";
 var pageCount = 0;
 
 var scrapeUrls = function(urls,pageTotal) {
-  var urlLeft, options, dbUrls=[];
+  var i, options, dbUrls=[];
   var controller = new EventEmitter();
   console.log(urls);
   options = {'reqPerSec': 0.2}; // Wait 5sec between each external request
-  var scrape = function (i){
-    scraper(urls[i], function(err, $, options) {
+  i = 0;
+    scraper(urls, function(err, $, options) {
       if (err) {throw err;}
       console.log('downloading url from',urls[i]);
       $('[itemprop="url"]').each(function() {
-        dbUrls.push([$(this).attr('href'), $(this).text().trim().replace(/[^A-Za-z\d/_]/g, '_')]);
+        dbUrls.push([$(this).attr('href'), $(this).text().trim().replace(/[^A-Za-z\d\_]/g, '_')]);
       });
       i ++;
       controller.emit('urlDownload', i);
     });
-  };
-  scrape(0);
   controller.on('urlDownload', function(i){
     console.log(i, 'pages downloaded');
     if (i >= urls.length) {
-      console.log('download url from all pages');
-      console.log(dbUrls);
-      fs.writeFile('./dataUrl.txt',dbUrls);
-       //   downLoadCsv(dbUrls);
-    } else {
-     setTimeout(function(){
-       scrape(i);
-     },2000);
+      console.log('download',dbUrls.length, 'urls from all pages');
+//      fs.writeFileSync('dataUrl.txt',dbUrls.join('\n'), function(err){
+//        if(err) throw err;
+//      });
+      downLoadCsv(dbUrls);
     }
   });
 };
