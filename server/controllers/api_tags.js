44c8c31fda320346bd336e30b602e7a6b9a33c12
pieteradejong <pieter.a.dejong@gmail.com>
2013-12-03 20:18:13 -0800

    fixed response sent by /tags endpoint

--- server/controllers/api_tags.js
+++ server/controllers/api_tags.js
@@ -5,16 +5,14 @@ apiControllers = {
     EmailToken = Models.EmailToken;
   },
 
-  getAllTags : function(req, res) {
+  'getAllTags' : function(req, res) {
     console.log("Retrieving all tags...");
     Metadata.Tags.all(function(err, result){
       if(err) {
-        res.writeHead(500);
-        res.end("500 Internal Server Error error:", err);
+        res.send("500 Internal Server Error error:", err);
       } else {
         console.log("Successfully retrieved all tags.");
-        res.writeHead(200);
-        res.render('', {}); // create new view?
+        res.send(result);
       }
     });
   }  
