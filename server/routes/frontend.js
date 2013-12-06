073c0afdc84bd1989d4fe539aad5413e851dc31e
shanshan <shanshan@shanshans-MacBook-Pro-2.local>
2013-12-05 16:39:06 -0800

    change the route name from tag to tags_leading to apiSearchTags

--- server/routes/frontend.js
+++ server/routes/frontend.js
@@ -40,6 +40,6 @@ module.exports = function(app, passport, Models, schema){
   app.get('/signup/:token', frontend.userSignupVerify);
   app.get('/userTableMetaData', frontend.userTableMetaData);
   app.get('/search/meta', frontend.apiSearchMeta);
-  app.get('/search/tag', frontend.apiSearchTags);
+  app.get('/search/tags', frontend.apiSearchTags);
   app.get('/search/table', frontend.apiSearchTable);
 };
